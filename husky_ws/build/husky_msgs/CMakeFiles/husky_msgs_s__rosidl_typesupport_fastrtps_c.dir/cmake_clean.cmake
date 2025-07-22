728fe3773635f3bba8e81f9edc79871841a0ed2d
Gaming-Geek1 <harperer151@gmail.com>
2025-07-22 14:07:57 -0600

    Shit
    fart

---
+++ husky_ws/build/husky_msgs/CMakeFiles/husky_msgs_s__rosidl_typesupport_fastrtps_c.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/husky_msgs_s__rosidl_typesupport_fastrtps_c.dir/rosidl_generator_py/husky_msgs/_husky_msgs_s.ep.rosidl_typesupport_fastrtps_c.c.o"
+  "CMakeFiles/husky_msgs_s__rosidl_typesupport_fastrtps_c.dir/rosidl_generator_py/husky_msgs/_husky_msgs_s.ep.rosidl_typesupport_fastrtps_c.c.o.d"
+  "rosidl_generator_py/husky_msgs/husky_msgs_s__rosidl_typesupport_fastrtps_c.pdb"
+  "rosidl_generator_py/husky_msgs/husky_msgs_s__rosidl_typesupport_fastrtps_c.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/husky_msgs_s__rosidl_typesupport_fastrtps_c.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ husky_ws/build/husky_msgs/husky_msgs__py/CMakeFiles/husky_msgs__py.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "/home/harperer123/husky_ws/build/husky_msgs/rosidl_generator_py/husky_msgs/_husky_msgs_s.ep.rosidl_typesupport_c.c"
+  "/home/harperer123/husky_ws/build/husky_msgs/rosidl_generator_py/husky_msgs/_husky_msgs_s.ep.rosidl_typesupport_fastrtps_c.c"
+  "/home/harperer123/husky_ws/build/husky_msgs/rosidl_generator_py/husky_msgs/_husky_msgs_s.ep.rosidl_typesupport_introspection_c.c"
+  "/home/harperer123/husky_ws/build/husky_msgs/rosidl_generator_py/husky_msgs/msg/__init__.py"
+  "/home/harperer123/husky_ws/build/husky_msgs/rosidl_generator_py/husky_msgs/msg/_husky_status.py"
+  "/home/harperer123/husky_ws/build/husky_msgs/rosidl_generator_py/husky_msgs/msg/_husky_status_s.c"
+  "CMakeFiles/husky_msgs__py"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/husky_msgs__py.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
